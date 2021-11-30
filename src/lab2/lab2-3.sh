05f4291165d4856bf230ccf22b02fa0ee9577601
Lee-wk <92263514+Lee-wk@users.noreply.github.com>
2021-11-30 19:44:00 +0900

    Update lab2-3.sh

--- src/lab2/lab2-3.sh
+++ src/lab2/lab2-3.sh
@@ -2,7 +2,7 @@
  
 bmi=`expr 10000 \* $1 / $2 / $2`
 
-if [ ${bmi} -le 18 ]; then
+if [ 1 -eq "$(echo "$bmi < 18.5"| bc)" ]; then
 	echo "Low weight"
 elif [ ${bmi} -lt 23 ]; then
         echo "Normal weight"
